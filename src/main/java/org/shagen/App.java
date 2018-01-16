09bcb82fb12194214072c087879ecbdca4f59ce2
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-01-16 12:54:15 +0100

    initial commit

---
+++ src/main/java/org/shagen/App.java
@@ -0,0 +1,12 @@
+package org.shagen;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class App implements EntryPoint {
+	
+	public void onModuleLoad() {
+
+		Window.alert("THIS IS MINIMAL GWT APPLICATION");
+	}
+}
